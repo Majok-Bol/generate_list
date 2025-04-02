999fcc58d54c1a9129842fc5b2a342807a9567f0
Majok-Bol <bolmajok001@gmail.com>
2025-04-02 18:32:09 +0300

    made list of 100 items

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,17 @@ class MyApp extends StatelessWidget{
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Text('Generate list items',style: TextStyle(color: Colors.white,fontSize: 25),),centerTitle: true,backgroundColor: Colors.purple,),
+        body: Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+          child:ListView.builder(
+            itemCount: 100,
+              
+              itemBuilder: (context,index){
+              return SizedBox(child: ListTile(leading:Icon(Icons.star,color: Colors.blueGrey,),title: Text('$index'),subtitle: Text('List of item'),));
+            
+          }),
+        ),
       ),
     );
   }
